9aee5091563d1ec2732ab9ad683546dfd3f15f3d
Jordan <jordansne@gmail.com>
2018-06-16 19:47:53 -0400

    Correct ESLint indentation error

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
  * @license MIT
  */
 
-const secretData = require('./config/secret'),
-      setup      = require('./config/setup');
+const secretData = require('./config/secret');
+const setup      = require('./config/setup');
 
 const Main = require('./src/main.js');
 const main = new Main(secretData, setup);
